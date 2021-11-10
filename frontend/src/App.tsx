b3dec5e33c096e143e6018b8a32c65d00d931a30
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-11-10 14:03:54 -0500

    fixed infinite rendering bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,14 @@ import Homepage from './Components/Homepage';
 function App() {
   const [socket, setSocket] = useState<Socket | undefined>();
 
-
- 
+  // @ts-ignore
   useEffect(() => {
+    console.log("rendering")
     const s = io("http://localhost:5000")
     setSocket(s)
-    return () => {
-      s.disconnect()
-      return undefined
-    }
+    return () => s.disconnect()
 
-  }, [socket, setSocket])
+  }, [setSocket])
 
 
   const SendText = (text: string) => {
